c0c660ac01518e18717d1fea615b126ca793ca7c
aleksandra.godes <san_shine7@list.ru>
2026-03-23 06:42:12 +0500

    commit

--- Week1/Day2/ExerciseXP/xp.py
+++ Week1/Day2/ExerciseXP/xp.py
@@ -7,3 +7,28 @@
 #Hello world
 
 print("Hello world\n" * 4)
+
+#Exercise 2: Some Math
+#Instructions
+#Write code that calculates the result of:
+#(99^3)*8 (meaning 99 to the power of 3, times 8).
+print((99^3)*8 )
+#768
+
+#Exercise 3: What is the output?
+#Instructions
+#Predict the output of the following code snippets:
+#Coment what is your guess, then run the code and compare
+#Example:
+#>>> 15 < 8 #False
+#>>> 5 < 3 #False
+#>>> 3 == 3 #True
+#>>> 3 == "3" #False
+#>>> "3" > 3 #error
+#>>> "Hello" == "hello" #False
+
+#False
+#True
+#False
+#error
+#False
