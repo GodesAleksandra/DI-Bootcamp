d759be9a41f4f589af3980271cad7267037aecc9
aleksandra.godes <san_shine7@list.ru>
2026-03-23 21:31:12 +0500

    commit

--- Week1/Day2/ExerciseXP/xpGold.py
+++ Week1/Day2/ExerciseXP/xpGold.py
@@ -11,3 +11,24 @@
 #I love python
 
 print("Hello world\n"*4+"I love python\n"*4)
+
+#Exercise 2 : What is the Season ?
+#Instructions
+#Ask the user to input a month (1 to 12).
+#Display the season of the month received :
+#Spring runs from March (3) to May (5)
+#Summer runs from June (6) to August (8)
+#Autumn runs from September (9) to November (11)
+#Winter runs from December (12) to February (2)
+
+month = input("Please input the month")
+if month in "3,4,5":
+    season = "Spring"
+elif month in "6,7,8":
+    season = "Summer"
+elif month in "9,10,11":
+    season = "Autumn"
+elif month in "12,1,2":
+    season = "Winter"
+
+print(season)
