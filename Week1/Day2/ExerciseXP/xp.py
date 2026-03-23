65865b1152adf671c6031b2bbbf652bae4f54f03
aleksandra.godes <san_shine7@list.ru>
2026-03-23 07:02:13 +0500

    commit

--- Week1/Day2/ExerciseXP/xp.py
+++ Week1/Day2/ExerciseXP/xp.py
@@ -68,3 +68,20 @@ b = 15
 if a > b:
     print("Hello World")
 
+#Exercise 7: Odd or Even
+#Instructions
+#Write code that asks the user for a number and determines whether this number is odd or even.
+
+
+
+#Exercise 8: What’s your name?
+#Instructions
+#Write code that asks the user for their name and determines whether or not you have the same name. Print out a funny message based on the outcome.
+
+
+
+#Exercise 9: Tall enough to ride a roller coaster
+#Instructions
+#Write code that will ask the user for their height in centimeters.
+#If they are over 145 cm, print a message that states they are tall enough to ride.
+#If they are not tall enough, print a message that says they need to grow some more to ride.
