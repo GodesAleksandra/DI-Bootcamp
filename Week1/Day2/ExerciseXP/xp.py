dfece3d9b7f9e8c3012a83982a030c457e2abaa2
aleksandra.godes <san_shine7@list.ru>
2026-03-23 06:56:44 +0500

    commit

--- Week1/Day2/ExerciseXP/xp.py
+++ Week1/Day2/ExerciseXP/xp.py
@@ -56,3 +56,15 @@ age = 30
 shoe_size = 36
 info = f"{name} aged {age} with shoe size {shoe_size} likes to buy Kinder surprise egg."
 print(info)
+
+#Exercise 6: A & B
+#Instructions
+#Create two variables, a and b.
+#Each variable’s value should be a number.
+#If a is bigger than b, have your code print "Hello World".
+
+a = 20
+b = 15
+if a > b:
+    print("Hello World")
+
