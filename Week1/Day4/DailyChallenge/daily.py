11686953750bffb3c010755926de01a67c2de37b
aleksandra.godes <san_shine7@list.ru>
2026-03-26 08:57:27 +0500

    commit

---
+++ Week1/Day4/DailyChallenge/daily.py
@@ -0,0 +1,17 @@
+#Challenge 1: Multiples of a Number
+#1. Ask the user for two inputs:
+#A number (integer).
+#A length (integer).
+#2. Create a program that generates a list of multiples of the given number.
+#3. The list should stop when it reaches the length specified by the user.
+
+
+
+#Challenge 2: Remove Consecutive Duplicate Letters
+#1. Ask the user for a string.
+#2. Write a program that processes the string to remove consecutive duplicate letters.
+#The new string should only contain unique consecutive letters.
+#For example, “ppoeemm” should become “poem” (removes consecutive duplicates like ‘pp’, ‘ee’, and ‘mm’).
+#3. The program should print the modified string.
+#The final string will not include any consecutive duplicates, but non-consecutive duplicates are allowed.
+#Example: In "recursive", the two ‘r’s and two ‘e’s are allowed because they are not consecutive.
