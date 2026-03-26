d2b27f9b508c4a97c4123f0e0532ff1f89863177
aleksandra.godes <san_shine7@list.ru>
2026-03-26 09:10:56 +0500

    commit

--- Week1/Day4/ExerciseXP/xp.py
+++ Week1/Day4/ExerciseXP/xp.py
@@ -53,6 +53,8 @@ print(basket)
 my_list = sorted(list(range(1,6)) + [x + 0.5 for x in range(1,5)])
 print(my_list)
 
+#my_list = [1.5 + i * 0.5 for i in range(8)]
+
 #Exercise 5: For Loop
 #Write a for loop to print all numbers from 1 to 20, inclusive.
 #Write another for loop that prints every number from 1 to 20 where the index is even.
