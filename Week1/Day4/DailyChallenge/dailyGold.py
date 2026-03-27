410b6d8dd38a9fd2ea98c0246e2d9ea2243714ff
aleksandra.godes <san_shine7@list.ru>
2026-03-27 20:41:18 +0500

    commit

---
+++ Week1/Day4/DailyChallenge/dailyGold.py
@@ -0,0 +1,13 @@
+#Ask the user for their birthdate (specify the format, for example: DD/MM/YYYY).
+#Display a little cake
+#The number of candles on the cake should be the last number of the users age, if they are 53, then add 3 candles.
+#Bonus : If they were born on a leap year, display two cakes !
+
+
+print(f'       ___iiiii___
+      |:H:a:p:p:y:|
+    __|___________|__
+   |^^^^^^^^^^^^^^^^^|
+   |:B:i:r:t:h:d:a:y:|
+   |                 |
+   ~~~~~~~~~~~~~~~~~~~')
