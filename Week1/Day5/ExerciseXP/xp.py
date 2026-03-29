b568a0d8e7440887503e2b84e9b4ed420a1f88f0
aleksandra.godes <san_shine7@list.ru>
2026-03-29 11:13:16 +0500

    commit

--- Week1/Day5/ExerciseXP/xp.py
+++ Week1/Day5/ExerciseXP/xp.py
@@ -108,3 +108,26 @@ more_on_zara = {
 
 brand.update(more_on_zara)
 print(brand)
+
+#Exercise 4: Disney Characters
+#You are given a list of Disney characters. Create three dictionaries based on different patterns as shown below:
+#Character List:
+#users = ["Mickey", "Minnie", "Donald", "Ariel", "Pluto"]
+#Expected Results:
+#1. Create a dictionary that maps characters to their indices:
+#{"Mickey": 0, "Minnie": 1, "Donald": 2, "Ariel": 3, "Pluto": 4}
+#2. Create a dictionary that maps indices to characters:
+#{0: "Mickey", 1: "Minnie", 2: "Donald", 3: "Ariel", 4: "Pluto"}
+#3. Create a dictionary where characters are sorted alphabetically and mapped to their indices:
+#{"Ariel": 0, "Donald": 1, "Mickey": 2, "Minnie": 3, "Pluto": 4}
+
+users = ["Mickey", "Minnie", "Donald", "Ariel", "Pluto"]
+
+user_to_idx = {user: i for i, user in enumerate(users)}
+print(user_to_idx)
+
+idx_to_user = {i: user for i, user in enumerate(users)}
+print(idx_to_user)
+
+sort_user_to_idx = {user: i for i, user in enumerate(sorted(users))}
+print(sort_user_to_idx)
