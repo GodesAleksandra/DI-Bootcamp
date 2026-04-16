8db1191428dc7acc00ae054afc130fa4d7aed103
aleksandra.godes <san_shine7@list.ru>
2026-04-17 04:06:54 +0500

    commit

--- Week24/Day2/ExerciseXP/xp.py
+++ Week24/Day2/ExerciseXP/xp.py
@@ -106,5 +106,32 @@ print(dog3.fight(dog2))
 #Step 3: Test PetDog Methods
 #Create instances of the PetDog class and test the train(), play(*args), and do_a_trick() methods.
 
+# In a new file dog.py
 
+from dog import Dog
+import random
+
+class PetDog(Dog):
+    def __init__(self, name, age, weight):
+        super().__init__(name, age, weight)
+        self.trained = False
+
+    def train(self):
+        print(self.bark())
+        self.trained = True
+
+    def play(self, *dog_names):
+        all_dogs = ", ".join(dog_names)
+        return f"{all_dogs} all play together"
+
+    def do_a_trick(self):
+        if self.trained:
+            tricks = ["does a barrel roll", "stands on his back legs", "shakes your hand", "plays dead"]
+            print(f"{self.name} {random.choice(tricks)}")
+
+# Test PetDog methods
+my_dog = PetDog("Fido", 2, 10)
+my_dog.train()
+my_dog.play("Buddy", "Max")
+my_dog.do_a_trick()
 
