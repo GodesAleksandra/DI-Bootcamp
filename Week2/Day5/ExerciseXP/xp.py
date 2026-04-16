85d87b00d1e08a2acc55d9fc501e444f8dfab105
aleksandra.godes <san_shine7@list.ru>
2026-04-16 20:16:50 +0500

    commit

--- Week2/Day5/ExerciseXP/xp.py
+++ Week2/Day5/ExerciseXP/xp.py
@@ -88,4 +88,23 @@ print(dog1.bark())
 print(dog2.run_speed())
 print(dog3.fight(dog2))
 
+#Exercise 3: Dogs Domesticated
+#Goal: Create a PetDog class that inherits from Dog and adds training and tricks.
+#Step 1: Import the Dog Class
+#In a new Python file, import the Dog class from the previous exercise.
+#Step 2: Create the PetDog Class
+#Create a class called PetDog that inherits from the Dog class.
+#Add a trained attribute to the __init__ method, with a default value of False.
+#trained means that the dog is trained to do some tricks.
+#Implement a train() method that prints the output of bark() and sets trained to True.
+#Implement a play(*args) method that prints “<dog_names> all play together”.
+#*args on this method is a list of dog instances.
+#Implement a do_a_trick() method that prints a random trick if trained is True.
+#Use this list for the ramdom tricks:
+#tricks = ["does a barrel roll", "stands on his back legs", "shakes your hand", "plays dead"]
+#Choose a random index from it each time the method is called.
+#Step 3: Test PetDog Methods
+#Create instances of the PetDog class and test the train(), play(*args), and do_a_trick() methods.
+
+
 
