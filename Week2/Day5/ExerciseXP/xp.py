df3ecc27005a66b6ce3fc7285272cbad1fc7a649
aleksandra.godes <san_shine7@list.ru>
2026-04-16 19:16:36 +0500

    commit

--- Week2/Day5/ExerciseXP/xp.py
+++ Week2/Day5/ExerciseXP/xp.py
@@ -48,3 +48,43 @@ all_cats = [Bengal("Alice",2), Chartreux("Baby",1), Siamese("Monika",5)]
 
 sara_pets = Pets(all_cats)
 sara_pets.walk()
+
+#Exercise 2: Dogs
+#Create a Dog class with methods for barking, running speed, and fighting.
+#Step 1: Create the Dog Class
+#Create a class called Dog with name, age, and weight attributes.
+#Implement a bark() method that returns “<dog_name> is barking”.
+#Implement a run_speed() method that returns weight / age * 10.
+#Implement a fight(other_dog) method that returns a string indicating which dog won the fight, based on run_speed * weight.
+#Step 2: Create Dog Instances
+#Create three instances of the Dog class with different names, ages, and weights.
+#Step 3: Test Dog Methods
+#Call the bark(), run_speed(), and fight() methods on the dog instances to test their functionality.
+
+class Dog:
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+
+    def bark(self):
+        return f'{self.name}  is barking'
+
+    def run_speed(self):
+        return self.weight / self.age * 10
+
+    def fight(self, other_dog):
+        if self.run_speed() * self.weight > other_dog.run_speed() * other_dog.weight:
+            return f'{self.name}  is won the fight'
+        else:
+            return f'{other_dog.name}  is won the fight'
+
+
+dog1 = Dog("Jo", 2, 15)
+dog2 = Dog("Baby", 1, 12)
+dog3 = Dog("Rich", 5, 18)
+
+# Step 3: Test dog methods
+print(dog1.bark())
+print(dog2.run_speed())
+print(dog3.fight(dog2))
