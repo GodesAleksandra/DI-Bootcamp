3a3914fe463c0a0e34e33db5446c47f52c30e3d1
aleksandra.godes <san_shine7@list.ru>
2026-04-26 04:36:30 +0500

    commit

--- Week3/Day2/ExerciseXP/xp.js
+++ Week3/Day2/ExerciseXP/xp.js
@@ -30,3 +30,36 @@ function displayNumbersDivisible(divisor) {
 displayNumbersDivisible(23);
 displayNumbersDivisible(3);
 displayNumbersDivisible(45);
+
+/* Exercise 2 : Shopping List
+Add the stock and prices objects to your js file.
+Create an array called shoppingList with the following items: “banana”, “orange”, and “apple”.
+It means that you have 1 banana, 1 orange and 1 apple in your cart.
+Create a function called myBill() that takes no parameters.
+The function should return the total price of your shoppingList. In order to do this you must follow these rules:
+The item must be in stock. (Hint : check out if .. in)
+If the item is in stock find out the price in the prices object.
+Call the myBill() function.
+Bonus: If the item is in stock, decrease the item’s stock by 1*/
+
+const stock = {
+    "banana": 6,
+    "apple": 0,
+    "pear": 12,
+    "orange": 32,
+    "blueberry":1
+}
+
+const prices = {
+    "banana": 4,
+    "apple": 2,
+    "pear": 1,
+    "orange": 1.5,
+    "blueberry":10
+}
+
+let shoppingList = ["banana", "orange", "apple"];
+
+function myBill() {
+
+}
