af5201d7c3dabedce1df59aa8fcb0d7539ba6cda
aleksandra.godes <san_shine7@list.ru>
2026-04-25 09:02:05 +0500

    commit

--- Week2/Day5/ExerciseXP/xp.js
+++ Week2/Day5/ExerciseXP/xp.js
@@ -36,4 +36,13 @@ for (let i = 0; i < people.length; i++) {
     }
 }
 
-console.log(people);
+/*Exercise 2 : Your favorite colors
+Create an array called colors where the value is a list of your five favorite colors.
+Loop through the array and as you loop console.log a string like so: “My #1 choice is blue”, “My #2 choice is red” ect… .
+Bonus: Change it to console.log “My 1st choice”, “My 2nd choice”, “My 3rd choice”, picking the correct suffix for each number.
+Hint : create an array of suffixes to do the Bonus*/
+
+colors = ["blue","green","yellow","violet","grey"]
+for (let i = 0; i < colors.length; i++) {
+    console.log("My #"+i+" choice is "+colors[i]);
+}
