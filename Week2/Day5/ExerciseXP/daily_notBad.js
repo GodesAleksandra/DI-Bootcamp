12c5dbb33f62aeaf436031a786316c44b939774a
aleksandra.godes <san_shine7@list.ru>
2026-04-25 22:20:04 +0500

    commit

--- Week2/Day5/ExerciseXP/daily_notBad.js
+++ Week2/Day5/ExerciseXP/daily_notBad.js
@@ -5,9 +5,17 @@ Create a variable called wordNot where it’s value is the first appearance (ie.
 Create a variable called wordBad where it’s value is the first appearance (ie. the position) of the substring “bad” (from the sentence variable).
 If the word “bad” comes after the word “not”, you should replace the whole “not…bad” substring with “good”, then console.log the result.
 For example, the result here should be : “The movie is good, I like it”
-If the word “bad” does not come after “not” or the words are not in the sentence, console.log the original sentence.*/
+If the word “bad” does not come after “not” or the words are not in the sentence, console.log the original sentence.
+Your string is : 'This dinner is not that bad ! You cook well',
+  --> the result is : 'This dinner is good ! You cook well'
 
-let sentence = "The movie is not that bad, I like it";
+  Your string is : 'This movie is not so bad !'
+  --> the result is : 'This movie is good !'
+
+  Your string is : 'This dinner is bad !'
+  --> the result is : 'This dinner is bad !'*/
+
+let sentence = "This dinner is not that bad ! You cook well";
 let wordNot = sentence.indexOf("not");
 let wordBad = sentence.indexOf("bad");
 let new_sentence = '';
