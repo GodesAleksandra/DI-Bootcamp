7c951f6ed773141608e83409221f215a84c4c7eb
aleksandra.godes <san_shine7@list.ru>
2026-04-28 09:27:48 +0500

    commit

--- Week3/Day2/ExerciseXP/xp.js
+++ Week3/Day2/ExerciseXP/xp.js
@@ -116,3 +116,57 @@ function changeEnough(itemPrice, amountOfChange) {
 changeEnough(14.11, [2,100,0,0])
 changeEnough(0.75, [0,0,20,5])
 changeEnough(4.25, [25, 20, 5, 0])
+
+/*Exercise 4 : Vacations Costs
+Let’s create functions that calculate your vacation’s costs:
+Define a function called hotelCost().
+It should ask the user for the number of nights they would like to stay in the hotel.
+If the user doesn’t answer or if the answer is not a number, ask again.
+The hotel costs $140 per night. The function should return the total price of the hotel.
+
+Define a function called planeRideCost().
+It should ask the user for their destination.
+If the user doesn’t answer or if the answer is not a string, ask again.
+The function should return a different price depending on the location.
+“London”: 183$
+“Paris” : 220$
+All other destination : 300$
+
+Define a function called rentalCarCost().
+It should ask the user for the number of days they would like to rent the car.
+If the user doesn’t answer or if the answer is not a number, ask again.
+Calculate the cost to rent the car. The car costs $40 everyday.
+If the user rents a car for more than 10 days, they get a 5% discount.
+The function should return the total price of the car rental.
+
+Define a function called totalVacationCost() that returns the total cost of the user’s vacation by calling the 3 functions that you created above.
+Example : The car cost: $x, the hotel cost: $y, the plane tickets cost: $z.
+Hint: You have to call the functions hotelCost(), planeRideCost() and rentalCarCost() inside the function totalVacationCost().
+
+Call the function totalVacationCost()
+
+Bonus: Instead of using a prompt inside the 3 first functions, only use a prompt inside the totalVacationCost() function. You need to change the 3 first functions, accordingly.*/
+
+function hotelCost() {
+    let nights_amount;
+
+    while (isNaN(nights_amount) || !nights_amount) {
+        nights_amount = Number(prompt('Please enter a number of nights you would like to stay in the hotel'));
+    }
+    return nights_amount * 140;
+}
+
+function planeRideCost() {
+    let destination, plane_cost;
+
+    while ((typeof destination !=== "string") || !destination) {
+        destination = prompt('Please enter your destination').toUpperCase();
+    }
+    if (destination === "LONDON") {
+        plane_cost = 183;
+    } else if (destination === "PARIS")
+        plane_cost = 220;
+    } else plane_cost = 300;
+    return plane_cost;
+}
+
