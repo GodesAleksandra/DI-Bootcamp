2a5f442598fa5ca6a4d01c8098b4091b1e54dd98
aleksandra.godes <san_shine7@list.ru>
2026-04-28 09:57:39 +0500

    1

---
+++ Week3/Day2/ExerciseXP/users.js
@@ -0,0 +1,32 @@
+/*Exercise 5 : Users
+Create a new structured HTML file and a new Javascript file
+<div id="container">Users:</div>
+<ul class="list">
+    <li>John</li>
+    <li>Pete</li>
+</ul>
+<ul class="list">
+    <li>David</li>
+    <li>Sarah</li>
+    <li>Dan</li>
+</ul>
+Add the code above, to your HTML file
+
+Using Javascript:
+Retrieve the div and console.log it
+Change the name “Pete” to “Richard”.
+Delete the second <li> of the second <ul>.
+Change the name of the first <li> of each <ul> to your name. (Hint : use a loop)
+
+Using Javascript:
+Add a class called student_list to both of the <ul>'s.
+Add the classes university and attendance to the first <ul>.
+
+Using Javascript:
+Add a “light blue” background color and some padding to the <div>.
+Do not display the <li> that contains the text node “Dan”. (the last <li> of the first <ul>)
+Add a border to the <li> that contains the text node “Richard”. (the second <li> of the <ul>)
+Change the font size of the whole body.
+
+Bonus: If the background color of the div is “light blue”, alert “Hello x and y” (x and y are the users in the div).
+*/
