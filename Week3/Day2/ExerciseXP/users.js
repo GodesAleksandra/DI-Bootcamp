619d3b546ac4b3fbe3c29b18e41d41fb1cc7ea46
aleksandra.godes <san_shine7@list.ru>
2026-04-29 13:32:08 +0500

    1

--- Week3/Day2/ExerciseXP/users.js
+++ Week3/Day2/ExerciseXP/users.js
@@ -33,5 +33,5 @@ for (let elem of tag_ul) {
 tag_ul[0].classList.add("university","attendance");
 
 let div = document.getElementById('container');
-div.style.background = 'blue';
+div.style.background = 'lightblue';
 div.style.padding = '10px';
