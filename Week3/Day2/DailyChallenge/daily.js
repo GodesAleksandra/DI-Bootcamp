e74a0cab3c031d33a8b29efe8ee9917ad54b221f
aleksandra.godes <san_shine7@list.ru>
2026-04-30 12:36:41 +0500

    1

--- Week3/Day2/DailyChallenge/daily.js
+++ Week3/Day2/DailyChallenge/daily.js
@@ -22,6 +22,7 @@ planets.forEach((planet, index) => {
     const div_planet = document.createElement("div");
     div_planet.classList.add("planet",planet.color);
     div_planet.style.background = planet.color;
+    div_planet.textContent = planet.name;
     section.appendChild(div_planet);
 });
 
