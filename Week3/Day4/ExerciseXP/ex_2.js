d69c981d4c35100ebcb7904b84b4fafdf8d42ba1
aleksandra.godes <san_shine7@list.ru>
2026-05-02 17:28:35 +0500

    1

---
+++ Week3/Day4/ExerciseXP/ex_2.js
@@ -0,0 +1,16 @@
+/*Exercise 2 : Work with forms
+Retrieve the form and console.log it.
+Retrieve the inputs by their id and console.log them.
+Retrieve the inputs by their name attribute and console.log them.
+When the user submits the form (ie. submit event listener)
+    use event.preventDefault(), why ?
+    get the values of the input tags,
+    make sure that they are not empty,
+    create an li per input value,
+    then append them to a the <ul class="usersAnswer"></ul>, below the form.
+The output should be :
+
+<ul class="usersAnswer">
+    <li>first name of the user</li>
+    <li>last name of the user</li>
+</ul>*/
