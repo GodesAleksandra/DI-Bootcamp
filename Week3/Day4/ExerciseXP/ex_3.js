6ec578e5fbf27487149a88049ab479311e650488
aleksandra.godes <san_shine7@list.ru>
2026-05-03 08:42:12 +0500

    1

--- Week3/Day4/ExerciseXP/ex_3.js
+++ Week3/Day4/ExerciseXP/ex_3.js
@@ -3,8 +3,8 @@ Declare a global variable named allBoldItems.
 Create a function called getBoldItems() that takes no parameter.
 This function should collect all the bold items inside the paragraph and assign them to the allBoldItems variable.
 Create a function called highlight() that changes the color of all the bold text to blue.
-
 Create a function called returnItemsToDefault() that changes the color of all the bold text back to black.
+
 Call the function highlight() on mouseover (ie. when the mouse pointer is moved onto the paragraph),
 and the function returnItemsToDefault() on mouseout (ie. when the mouse pointer is moved out of the paragraph).*/
 
@@ -22,8 +22,10 @@ function highlight() {
     });
 }
 
-/*function returnItemsToDefault() {
-
-}*/
+function returnItemsToDefault() {
+    allBoldItems.forEach(p => {
+      p.style.color = "black";
+    });
+}
 
 highlight();
