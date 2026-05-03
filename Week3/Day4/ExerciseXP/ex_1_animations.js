f1d611320333e41c3543c21c7a48e38de13979e8
aleksandra.godes <san_shine7@list.ru>
2026-05-03 13:38:56 +0500

    1

--- Week3/Day4/ExerciseXP/ex_1_animations.js
+++ Week3/Day4/ExerciseXP/ex_1_animations.js
@@ -12,29 +12,22 @@ The interval will be cleared (ie. clearInterval), when the user will click on th
 Instead of clicking on the button, the interval will be cleared (ie. clearInterval) as soon as there will be 5 paragraphs inside the <div id="container">.
 */
 
-/*function Greeting() {
+function Greeting() {
     alert("Hello World");
 }
-setTimeout(Greeting, 2000);*/
+setTimeout(Greeting, 2000);
 
 function AddParagraph() {
     p = document.createElement("p");
     p.textContent = "Hello World";
     document.getElementById("container").appendChild(p);
 }
-//setTimeout(AddParagraph, 2000);
-
-/*let timer = setInterval(AddParagraph(), 2000);*/
-/*let btn_clearInt = document.getElementById("clear");
-btn_clearInt.addEventListener("click", clearInterval(timer));*/
+setTimeout(AddParagraph, 2000);
 
 const timer = setInterval(() => {
-  AddParagraph();
   if (document.querySelectorAll('p').length > 5) stopTimer();
+  AddParagraph();
 }, 2000);
-
-//const btn_clearInt = document.getElementById("clear");
-//btn_clearInt.addEventListener("click", clearInterval(timer));
 
 function stopTimer() {
   clearInterval(timer);
