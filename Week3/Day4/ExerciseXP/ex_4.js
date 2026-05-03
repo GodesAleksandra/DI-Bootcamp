f51dc1de16f31aa90d5d7323cbe9d9fb32004ca9
aleksandra.godes <san_shine7@list.ru>
2026-05-03 09:13:40 +0500

    1

--- Week3/Day4/ExerciseXP/ex_4.js
+++ Week3/Day4/ExerciseXP/ex_4.js
@@ -1,3 +1,13 @@
 /*Exercice 4 : Volume of a sphere
-Write a JavaScript program to calculate the volume of a sphere/*
+Write a JavaScript program to calculate the volume of a sphere*/
 
+const myForm = document.getElementById('MyForm');
+const volume = document.getElementById('volume');
+
+myForm.addEventListener("submit", calculateVolume);
+function calculateVolume(event){
+    event.preventDefault(); //prevent the default form submission action using JavaScript to implement custom actions such as form validation before the form data is sent to the server
+    let formData = new FormData(event.target);
+    let radius = formData.get('radius');
+    volume.value = radius;
+}
