d110cd7e08957e42fbb44417f92333443dbb5b6d
aleksandra.godes <san_shine7@list.ru>
2026-05-04 21:03:48 +0500

    1

--- Week4/Day2/ExerciseXP/xp.js
+++ Week4/Day2/ExerciseXP/xp.js
@@ -88,3 +88,20 @@ alert(`outside of the if block ${a}`);
 // with const instead of let ?
 // in the if block 5
 // outside of the if block 2
+
+/*Exercise 2 : Ternary operator
+function winBattle(){
+    return true;
+}
+Transform the winBattle() function to an arrow function.
+Create a variable called experiencePoints.
+Assign to this variable, a ternary operator. If winBattle() is true, the experiencePoints variable should be equal to 10, else the variable should be equal to 1.
+Console.log the experiencePoints variable.*/
+
+const winBattle = () => {
+    return true;
+}
+
+const experiencePoints = winBattle() == true ? 10 : 1;
+console.log(experiencePoints);
+
