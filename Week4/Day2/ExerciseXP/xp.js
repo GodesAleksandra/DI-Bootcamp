b2e97d630f2b491cc5284c5a1b4e1fdb8f83e43a
aleksandra.godes <san_shine7@list.ru>
2026-05-04 21:32:34 +0500

    1

--- Week4/Day2/ExerciseXP/xp.js
+++ Week4/Day2/ExerciseXP/xp.js
@@ -153,3 +153,7 @@ convertToGr(20);
 const convert = (x) => x * 1000;
 convert(5);
 
+/*Exercise 6 : Fortune teller
+Create a self invoking function that takes 4 arguments: number of children, partner’s name, geographic location, job title.
+The function should display in the DOM a sentence like "You will be a <job title> in <geographic location>,
+and married to <partner's name> with <number of children> kids."*/
