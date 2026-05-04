d4f9c8486476b12aaacb389d759e7830bc9a43df
aleksandra.godes <san_shine7@list.ru>
2026-05-04 21:05:14 +0500

    1

--- Week4/Day2/ExerciseXP/xp.js
+++ Week4/Day2/ExerciseXP/xp.js
@@ -105,3 +105,12 @@ const winBattle = () => {
 const experiencePoints = winBattle() == true ? 10 : 1;
 console.log(experiencePoints);
 
+/* Exercise 3 : Is it a string ?
+Write a JavaScript arrow function that checks whether the value of the argument passed, is a string or not. The function should return true or false
+Check out the example below to see the expected output
+Example:
+console.log(isString('hello'));
+//true
+console.log(isString([1, 2, 4, 0]));
+//false
+*/
