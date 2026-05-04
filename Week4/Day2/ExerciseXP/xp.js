d98164dff9023c92a8959f0cb73fd958c3c266e2
aleksandra.godes <san_shine7@list.ru>
2026-05-04 21:49:56 +0500

    1

--- Week4/Day2/ExerciseXP/xp.js
+++ Week4/Day2/ExerciseXP/xp.js
@@ -157,3 +157,7 @@ convert(5);
 Create a self invoking function that takes 4 arguments: number of children, partner’s name, geographic location, job title.
 The function should display in the DOM a sentence like "You will be a <job title> in <geographic location>,
 and married to <partner's name> with <number of children> kids."*/
+
+(function (name) {
+    alert("Hello " + name);
+})("Sarah")
