94167ae510e104955a38ed752b27252eee25c1b0
aleksandra.godes <san_shine7@list.ru>
2026-05-05 10:52:13 +0500

    1

---
+++ Week4/Day2/ExerciseXP/ex_7.js
@@ -0,0 +1,15 @@
+/*Exercise 7 : Welcome
+John has just signed in to your website and you want to welcome him.
+Create a Navbar in your HTML file.
+In your js file, create a self invoking function that takes 1 argument: the name of the user that just signed in.
+The function should add a div in the nabvar, displaying the name of the user and his profile picture.*/
+
+(function (user) {
+    const div = document.createElement("div");
+    p.textContent = user;
+    document.getElementById("nabvar").appendChild(div);
+    const img = document.createElement('img');
+    img.src = "profile_picture.png";
+    img.width = 100;
+    p.appendChild(img);
+})("Anna");
