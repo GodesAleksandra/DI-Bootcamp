627068d44e84010031446dac9cecaa3924089846
aleksandra.godes <san_shine7@list.ru>
2026-05-05 10:58:19 +0500

    1

--- Week4/Day2/ExerciseXP/xp.js
+++ Week4/Day2/ExerciseXP/xp.js
@@ -171,3 +171,19 @@ In your js file, create a self invoking function that takes 1 argument: the name
 The function should add a div in the nabvar, displaying the name of the user and his profile picture.*/
 
 //ex_7.js;
+
+/* Exercise 8 : Juice Bar
+You will use nested functions, to open a new juice bar.
+Part I:
+The outer function named makeJuice receives 1 argument: the size of the beverage the client wants - small, medium or large.
+The inner function named addIngredients receives 3 ingredients, and displays on the DOM a sentence like The client wants a <size drink> juice,
+containing <first ingredient>, <second ingredient>, <third ingredient>".
+Invoke the inner function ONCE inside the outer function. Then invoke the outer function in the global scope.
+Part II:
+In the makeJuice function, create an empty array named ingredients.
+The addIngredients function should now receive 3 ingredients, and push them into the ingredients array.
+Create a new inner function named displayJuice that displays on the DOM a sentence like The client wants a <size drink> juice,
+containing <first ingredient>, <second ingredient>, <third ingredient>".
+The client wants 6 ingredients in his juice, therefore, invoke the addIngredients function TWICE.
+Then invoke once the displayJuice function. Finally, invoke the makeJuice function in the global scope.*/
+
