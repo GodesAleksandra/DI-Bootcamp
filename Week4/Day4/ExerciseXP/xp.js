ef13e5a9c3417c2e2e1175d3ac91554222857a97
aleksandra.godes <san_shine7@list.ru>
2026-05-09 09:34:23 +0500

    1

--- Week4/Day4/ExerciseXP/xp.js
+++ Week4/Day4/ExerciseXP/xp.js
@@ -28,12 +28,13 @@ const fruits = ["apple", "orange"];
 const vegetables = ["carrot", "potato"];
 
 const result = ['bread', ...vegetables, 'chicken', ...fruits];
-console.log(result);
+console.log(result);// Output: ['bread', 'carrot', 'potato', 'chicken', 'apple', 'orange']
 
 //------2------
 const country = "USA";
-console.log([...country]);
+console.log([...country]);// Output: ['U', 'S', 'A']
 
 //------Bonus------
 let newArray = [...[,,]];
-console.log(newArray);
+console.log(newArray);// Output: [undefined, undefined]
+
