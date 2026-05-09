1c7781912cec87a1d21e387334b1f8a9bcb7685e
aleksandra.godes <san_shine7@list.ru>
2026-05-09 10:07:44 +0500

    1

--- Week4/Day4/ExerciseXP/xp.js
+++ Week4/Day4/ExerciseXP/xp.js
@@ -65,3 +65,21 @@ console.log(lastNameFSResidents);
 Using this array use the reduce() method to combine all of these into a single string.*/
 
 const epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
+
+let str = epic.reduce((acc, val) => {
+  return acc + ' ' + val;
+});
+
+console.log(str)
+
+/*Exercise 6 : Employees #2
+Using the filter() method, create a new array, containing the students that passed the course.
+Bonus : Chain the filter method with a forEach method, to congratulate the students with their name and course name (ie. “Good job Jenner, you passed the course in Information Technology”, “Good Job Marco you passed the course in Robotics” ect…)*/
+
+const students = [{name: "Ray", course: "Computer Science", isPassed: true},
+               {name: "Liam", course: "Computer Science", isPassed: false},
+               {name: "Jenner", course: "Information Technology", isPassed: true},
+               {name: "Marco", course: "Robotics", isPassed: true},
+               {name: "Kimberly", course: "Artificial Intelligence", isPassed: false},
+               {name: "Jamie", course: "Big Data", isPassed: false}];
+
