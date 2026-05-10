9fdab14b1aadcd42c5ba3493db5682ef733e2416
aleksandra.godes <san_shine7@list.ru>
2026-05-10 09:12:19 +0500

    1

--- Week4/Day5/ExerciseXP/xp.js
+++ Week4/Day5/ExerciseXP/xp.js
@@ -14,3 +14,18 @@ const person = {
 const {name, location: {country, city, coordinates: [lat, lng]}} = person;
 
 console.log(`I am ${name} from ${city}, ${country}. Latitude(${lat}), Longitude(${lng})`);
+//Output: I am John Doe from Vancouver, Canada. Latitude(49.2827), Longitude(-123.1207)
+
+/*Exercise 2: Display Student Info
+function displayStudentInfo(objUser){
+    //destructuring
+}
+displayStudentInfo({first: 'Elie', last:'Schoppik'});
+Using the code above, destructure the parameter inside the function and return a string as the example seen below:
+//output : 'Your full name is Elie Schoppik'*/
+
+function displayStudentInfo(objUser){
+    //destructuring
+}
+
+displayStudentInfo({first: 'Elie', last:'Schoppik'});
