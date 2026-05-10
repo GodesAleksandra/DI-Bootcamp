b80978bd5752acb087f3456d8c1afdea11ac7a61
aleksandra.godes <san_shine7@list.ru>
2026-05-10 09:51:44 +0500

    1

--- Week4/Day5/ExerciseXP/xp.js
+++ Week4/Day5/ExerciseXP/xp.js
@@ -25,7 +25,16 @@ Using the code above, destructure the parameter inside the function and return a
 //output : 'Your full name is Elie Schoppik'*/
 
 function displayStudentInfo(objUser){
-    //destructuring
+    console.log('Your full name is ' + objUser.first + ' ' + objUser.last);
 }
 
 displayStudentInfo({first: 'Elie', last:'Schoppik'});
+
+/* Exercise 3: User & id
+Using methods taught in class, turn the users object into an array:
+Excepted output: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
+FYI : The number is their ID number.
+Modify the outcome of part 1, by multipling the user’s ID by 2.
+Excepted output: [ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]*/
+
+const users = { user1: 18273, user2: 92833, user3: 90315 }
