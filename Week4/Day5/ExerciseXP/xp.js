d2efd5a43747af8da6571944c08fe5fb31445733
aleksandra.godes <san_shine7@list.ru>
2026-05-10 10:03:22 +0500

    1

--- Week4/Day5/ExerciseXP/xp.js
+++ Week4/Day5/ExerciseXP/xp.js
@@ -37,4 +37,12 @@ FYI : The number is their ID number.
 Modify the outcome of part 1, by multipling the user’s ID by 2.
 Excepted output: [ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]*/
 
-const users = { user1: 18273, user2: 92833, user3: 90315 }
+const users = { user1: 18273, user2: 92833, user3: 90315 };
+let arrUsers = Object.entries(users)
+console.log(arrUsers);
+
+arrUsers.forEach(user => {
+  user[1] = user[1] * 2;
+});
+console.log(arrUsers);
+
