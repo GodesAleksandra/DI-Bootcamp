c4a27518389e0133a03eb5d8cf952bb7bbfd88ba
aleksandra.godes <san_shine7@list.ru>
2026-05-13 09:44:41 +0500

    1

--- Week5/Day2/ExerciseXP/xp.js
+++ Week5/Day2/ExerciseXP/xp.js
@@ -72,8 +72,30 @@ let jsonMarioGamePr = JSON.stringify(marioGame, null, 2);
 
 /*In the debugger:
 Breakpoint on jsonMarioGame:
-
+"{"detail":"An amazing game!","characters":{"mario":{"description":"Small and jumpy. Likes princesses.","height":10,"weight":3,"speed":12},"bowser":{"description":"Big and green, Hates princesses.","height":16,"weight":6,"speed":4},"princessPeach":{"description":"Beautiful princess.","height":12,"weight":2,"speed":2}}}"
 
 Breakpoint on jsonMarioGamePr:
-
+"{
+  "detail": "An amazing game!",
+  "characters": {
+    "mario": {
+      "description": "Small and jumpy. Likes princesses.",
+      "height": 10,
+      "weight": 3,
+      "speed": 12
+    },
+    "bowser": {
+      "description": "Big and green, Hates princesses.",
+      "height": 16,
+      "weight": 6,
+      "speed": 4
+    },
+    "princessPeach": {
+      "description": "Beautiful princess.",
+      "height": 12,
+      "weight": 2,
+      "speed": 2
+    }
+  }
+}"
 */
