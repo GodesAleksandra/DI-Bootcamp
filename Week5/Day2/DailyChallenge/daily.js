b630d403e4d479b74e338454288077c00ca028fc
aleksandra.godes <san_shine7@list.ru>
2026-05-13 18:37:55 +0500

    1

---
+++ Week5/Day2/DailyChallenge/daily.js
@@ -0,0 +1,5 @@
+/*Daily challenge : HTML Form
+Create a form with two fields (name, last name) and a submit button.
+When you click the Send button, retrieve the data from the inputs, and append it on the DOM as a JSON string.
+*/
+
