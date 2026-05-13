5b8f708e4b37d8053c4afaddb496d664cfe3c873
aleksandra.godes <san_shine7@list.ru>
2026-05-13 18:47:33 +0500

    1

--- Week5/Day2/DailyChallenge/daily.js
+++ Week5/Day2/DailyChallenge/daily.js
@@ -3,3 +3,16 @@ Create a form with two fields (name, last name) and a submit button.
 When you click the Send button, retrieve the data from the inputs, and append it on the DOM as a JSON string.
 */
 
+form.addEventListener("submit", createJSON);
+function createJSON(event){
+    event.preventDefault();
+    let formData = new FormData(event.target);
+    let valFormJSON = "{";
+    formData.forEach(function(value, key) {
+        valFormJSON = valFormJSON + '"' + key + '":"' + value + '",';
+    });
+    valFormJSON = valFormJSON.slice(0, -1) + "}";
+    p = document.createElement("p");
+    p.textContent = valFormJSON;
+    document.body.appendChild(p);
+}
