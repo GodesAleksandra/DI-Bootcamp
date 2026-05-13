c03885a727658206bc722d8c83ef18f29d617c62
aleksandra.godes <san_shine7@list.ru>
2026-05-13 09:28:30 +0500

    1

--- Week5/Day2/ExerciseXP/xp.js
+++ Week5/Day2/ExerciseXP/xp.js
@@ -65,3 +65,7 @@ const marioGame = {
   },
 }
 
+let jsonMarioGame = JSON.stringify(marioGame);
+console.log(jsonMarioGame); //Output: {"detail":"An amazing game!","characters":{"mario":{"description":"Small and jumpy. Likes princesses.","height":10,"weight":3,"speed":12},"bowser":{"description":"Big and green, Hates princesses.","height":16,"weight":6,"speed":4},"princessPeach":{"description":"Beautiful princess.","height":12,"weight":2,"speed":2}}}
+//When you convert a JS object to JSON using JSON.stringify(), nested objects are automatically and recursively converted into JSON strings.
+
