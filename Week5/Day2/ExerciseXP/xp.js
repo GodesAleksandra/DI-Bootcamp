1529b931fc73ca154ad24ae3f8ddf65d88f92f5b
aleksandra.godes <san_shine7@list.ru>
2026-05-13 09:06:07 +0500

    1

---
+++ Week5/Day2/ExerciseXP/xp.js
@@ -0,0 +1,21 @@
+/* Exercise 1 : HTML Form
+form
+Create a form. The form should contain three inputs:
+a small text input asking for a name,
+a larger textarea input to write a message,
+a submit input (“Send”)
+When you click the Send button, the form will be submitted with a GET method. (you can use the same HTML file for the action url)
+Where will the sent data appear?*/
+
+/* When the element’s method attribute is GET (or is omitted), the information in the form is added to the end of the action URL as a query string:
+file:///C:/Users/1/Desktop/DI-Bootcamp/Week5/Day2/ExerciseXP/xp.html?name=Aleksandra&comments=no+comments
+In the Network tab:
+Request URL
+file:///C:/Users/1/Desktop/DI-Bootcamp/Week5/Day2/ExerciseXP/xp.html?name=Aleksandra&comments=no+comments
+Request Method
+GET
+
+Query String Parameter
+name  Aleksandra
+comments  no comments*/
+
