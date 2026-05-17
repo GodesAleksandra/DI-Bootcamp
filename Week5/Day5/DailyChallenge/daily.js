259c7803a11a0946e9c820c5159b86690e82e234
aleksandra.godes <san_shine7@list.ru>
2026-05-17 14:46:07 +0500

    1

--- Week5/Day5/DailyChallenge/daily.js
+++ Week5/Day5/DailyChallenge/daily.js
@@ -17,21 +17,31 @@ const makeAllCaps = (words) => {
   });
 };
 
+const sortWords = (words) => {
+  return new Promise((resolve, reject) => {
+    if(words.length > 4) {
+      resolve(words.sort());
+    } else {
+      reject(`The array [${words}] length is bigger than 4`);
+    }
+  });
+};
+
 //in this example, the catch method is executed
 makeAllCaps([1, "pear", "banana"])
-      //.then((arr) => sortWords(arr))
+      .then((arr) => sortWords(arr))
       .then((result) => console.log(result))
       .catch(error => console.log(error));
 
 //in this example, the catch method is executed
 makeAllCaps(["apple", "pear", "banana"])
-      //.then((arr) => sortWords(arr))
+      .then((arr) => sortWords(arr))
       .then((result) => console.log(result))
       .catch(error => console.log(error))
 
 //in this example, you should see in the console,
 // the array of words uppercased and sorted
 makeAllCaps(["apple", "pear", "banana", "melon", "kiwi"])
-      //.then((arr) => sortWords(arr))
+      .then((arr) => sortWords(arr))
       .then((result) => console.log(result)) //["APPLE","BANANA", "KIWI", "MELON", "PEAR"]
       .catch(error => console.log(error))
