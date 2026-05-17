33ef561b4e3bebcdeac85c7b61b864820ac00cd1
aleksandra.godes <san_shine7@list.ru>
2026-05-17 15:52:00 +0500

    1

--- Week5/Day5/DailyChallenge/daily.js
+++ Week5/Day5/DailyChallenge/daily.js
@@ -45,3 +45,115 @@ makeAllCaps(["apple", "pear", "banana", "melon", "kiwi"])
       .then((arr) => sortWords(arr))
       .then((result) => console.log(result)) //["APPLE","BANANA", "KIWI", "MELON", "PEAR"]
       .catch(error => console.log(error))
+
+/*2nd daily challenge
+Create three functions. The two first functions should return a promise..
+
+The first function is named toJs():
+this function converts the morse json string provided above to a morse javascript object.
+if the morse javascript object is empty, throw an error (use reject)
+else return the morse javascript object (use resolve)
+
+The second function called toMorse(morseJS), takes one argument: the new morse javascript object.
+This function asks the user for a word or a sentence.
+if the user entered a character that doesn’t exist in the new morse javascript object, throw an error. (use reject)
+else return an array with the morse translation of the user’s word.
+if the user enters the word "Hello", the promise resolves with this value ["....", ".", ".-..", ".-..","---"]
+if the user entered the word "¡Hola!", the promise rejects because the character "¡" doesn't exist in the morse javascript object
+
+
+The third function called joinWords(morseTranslation), takes one argument: the morse translation array
+
+this function joins the morse translation by using line break and display it on the page (ie. On the DOM)
+
+Chain the three functions.
+Example:
+"Hello" gives you
+....
+.
+.-..
+.-..
+---*/
+
+const morse = `{
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  "a": ".-",
+  "b": "-...",
+  "c": "-.-.",
+  "d": "-..",
+  "e": ".",
+  "f": "..-.",
+  "g": "--.",
+  "h": "....",
+  "i": "..",
+  "j": ".---",
+  "k": "-.-",
+  "l": ".-..",
+  "m": "--",
+  "n": "-.",
+  "o": "---",
+  "p": ".--.",
+  "q": "--.-",
+  "r": ".-.",
+  "s": "...",
+  "t": "-",
+  "u": "..-",
+  "v": "...-",
+  "w": ".--",
+  "x": "-..-",
+  "y": "-.--",
+  "z": "--..",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "!": "-.-.--",
+  "-": "-....-",
+  "/": "-..-.",
+  "@": ".--.-.",
+  "(": "-.--.",
+  ")": "-.--.-"
+}`;
+let morseJS = {};
+
+const toJs = (morse) => {
+  return new Promise((resolve, reject) => {
+    morseJS = JSON.parse(morse);
+    if(morseJS) {
+      resolve(morseJS);
+    } else {
+      reject(`morseJS is empty`);
+    }
+  });
+};
+
+/*The second function called toMorse(morseJS), takes one argument: the new morse javascript object.
+This function asks the user for a word or a sentence.
+if the user entered a character that doesn’t exist in the new morse javascript object, throw an error. (use reject)
+else return an array with the morse translation of the user’s word.
+if the user enters the word "Hello", the promise resolves with this value ["....", ".", ".-..", ".-..","---"]
+if the user entered the word "¡Hola!", the promise rejects because the character "¡" doesn't exist in the morse javascript object*/
+
+const toMorse = (morseJS) => {
+  return new Promise((resolve, reject) => {
+    const word = prompt("What is a word or a sentence?");
+    if(Object.keys(morseJS).includes(word)) {
+      resolve('Yes');
+    } else {
+      reject(`You entered a character that doesn’t exist in the morse object`);
+    }
+  });
+};
+
+toJs(morse)
+  .then((morseJS) => toMorse(morseJS))
+  .then((result) => console.log(result))
+  .catch(error => console.log(error));
