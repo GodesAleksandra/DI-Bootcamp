fb46a51be343648442b33641f7cc432566ce6b63
aleksandra.godes <san_shine7@list.ru>
2026-05-17 05:37:56 +0500

    1

--- Week5/Day5/ExerciseXP/xp.js
+++ Week5/Day5/ExerciseXP/xp.js
@@ -48,3 +48,13 @@ resolvingPromise.then((message) => {
 Use Promise.resolve(value) to create a promise that will resolve itself with a value of 3.
 Use Promise.reject(error) to create a promise that will reject itself with the string “Boo!”*/
 
+const resolvedPromise = Promise.resolve(3);
+const rejectedPromise = Promise.reject("Boo!");
+
+resolvedPromise.then(value => {
+  console.log(value);
+});
+
+rejectedPromise.catch(error => {
+  console.error(error);
+});
