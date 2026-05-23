86fc8d57a8b9d9a9bc8fb35f07cd8cfdeafe977d
aleksandra.godes <san_shine7@list.ru>
2026-05-23 21:28:50 +0500

    1

--- Week6/Day2/DailyChallenge/daily.js
+++ Week6/Day2/DailyChallenge/daily.js
@@ -10,7 +10,7 @@ formElem.onsubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(formElem);
     const category = formData.get('category'); // Matches the 'name' attribute in HTML
-    const url = 'https://api.giphy.com/v1/gifs/search?q='+category+'&limit=1&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My';
+    const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=1&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`;
     console.log(url);
 
     fetch(url)
