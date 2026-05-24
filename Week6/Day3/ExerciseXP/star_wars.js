be379f9fc8ef21dfcfd8e9d9fbc676ca270dee55
aleksandra.godes <san_shine7@list.ru>
2026-05-24 21:45:46 +0500

    1

--- Week6/Day3/ExerciseXP/star_wars.js
+++ Week6/Day3/ExerciseXP/star_wars.js
@@ -1,3 +1,10 @@
+let button = document.querySelector('#button');
+let names = document.querySelector('#name');
+let height = document.querySelector('#height');
+let gender = document.querySelector('#gender');
+let birthYear = document.querySelector('#birth-year');
+let homeWorld = document.querySelector('#home-world');
+
 const getStarWarsInfo = async () => {
     try {
         updateWithLoading();
@@ -5,15 +12,21 @@ const getStarWarsInfo = async () => {
         let apiUrl = 'https://www.swapi.tech/api/people/' + randomNumber + '/';
         const response = await fetch(apiUrl);
         if (!response.ok) {
-            throw new Error("Something went wrong")
+            //throw new Error("Something went wrong")
+            updateInfoWithError();
+            console.log('there was an error');
         } else {
             //console.log("response", response)
             let data = await response.json();
             //console.log("data", data);
-            let objectStarWars = data.result;
-            console.log(objectStarWars);
+            names.innerText = data.result.properties.name;
+            height.innerText = `Height: ${data.result.properties.height}`;
+            gender.innerText = `Gender: ${data.result.properties.gender}`;
+            birthYear.innerText = `Birth Year: ${data.result.properties.birth_year}`;
+            homeWorld.innerText = `Home World: ${data.result.properties.planet}`;
         }
     } catch (err) {
+        updateInfoWithError();
         console.log("In the catch ", err);
     }
 }
