8c6433393707c9c13298aad9f0e8a64e47b07a36
aleksandra.godes <san_shine7@list.ru>
2026-05-26 20:06:06 +0500

    1

--- Week7/Day2/ExerciseXP/xp.ts
+++ Week7/Day2/ExerciseXP/xp.ts
@@ -10,3 +10,24 @@ let age: number = 25;
 let name: string = "Alex";
 console.log(`Name: ${name}, Age: ${age}`);
 
+/*Exercise 3: Union Types
+Use union types to declare a variable id that can be either a string or a number.*/
+
+let id: string | number;
+
+/*Exercise 4: Control Flow with if...else
+Write a function that takes a number as input and returns a string indicating whether the number is positive, negative, or zero.
+Use if...else statements to control the flow of a program.*/
+
+
+function checkNumber(num: number): string {
+  if (num > 0) {
+    return "positive";
+  } else if (num < 0) {
+    return "negative";
+  } else {
+    return "zero";
+  }
+}
+
+checkNumber(2);
