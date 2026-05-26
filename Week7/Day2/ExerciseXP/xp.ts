172790145eff6198541d051f08f0ca9df5f27166
aleksandra.godes <san_shine7@list.ru>
2026-05-26 19:57:32 +0500

    1

--- Week7/Day2/ExerciseXP/xp.ts
+++ Week7/Day2/ExerciseXP/xp.ts
@@ -1,3 +1,12 @@
 /*Exercise 1: Hello, World! Program
 Create a TypeScript program that logs the message “Hello, World!” to the console.*/
+
 console.log("Hello, World!");
+
+/*Exercise 2: Type Annotations
+Define a variable age of type number and a variable name of type string, and log them to the console.*/
+
+let age: number = 25;
+let name: string = "Alex";
+console.log(`Name: ${name}, Age: ${age}`);
+
