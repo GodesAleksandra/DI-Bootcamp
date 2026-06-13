789dc454a8f6df91d9c68a68f5bb52b9cddaaf66
aleksandra.godes <san_shine7@list.ru>
2026-06-13 23:23:41 +0500

    1

---
+++ Week8/Day5/ExerciseXP/file-explorer/read-directory.js
@@ -0,0 +1,20 @@
+/*Exercise 7: Reading and Copying Files
+Create a directory named file-explorer.
+Inside the file-explorer directory, create a file named copy-file.js.
+In copy-file.js, use the fs module to read the content from a file named source.txt and then write the same content to a 
+new file named destination.txt.
+Create another file named read-directory.js.
+In read-directory.js, use the fs module to read the list of files in a specified directory and display their names in the terminal.
+Open a terminal in the file-explorer directory.
+Run node copy-file.js to copy the content from source.txt to destination.txt.
+Run node read-directory.js to list the files in the directory.*/
+
+const fs = require('fs');
+
+const directoryPath = './'; // Specify the directory you want to read
+const files = fs.readdirSync(directoryPath);
+console.log('Files in the directory:');
+files.forEach(file => {
+    console.log('- ' + file);
+});
+s
